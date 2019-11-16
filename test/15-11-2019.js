a1353b5779084edd19def77762f260ab77ff2c0a
CripBoy <cripboygit@gmail.com>
2019-11-16 01:04:37 -0300

    done

--- test/15-11-2019.js
+++ test/15-11-2019.js
@@ -1,18 +1,17 @@
 require('dotenv').config({path: "../.env"})
 var suap = require("../src");
 
-
 (async function()
 {
 	var token = await suap.auth(
 		process.env.MATRICULA, 
 		process.env.PASSWORD);
 
-	// var data = await suap.getData();
-	// var years = await suap.getYears();
-	// var notes = await suap.getNotes(2019, 1);
-	// var classes = await suap.getClasses(2019, 1);
-	var _class = await suap.getClass(48553);
+	var data = await suap.getData();
+	var years = await suap.getYears();
+	var notes = await suap.getNotes(2019, 1);
+	var classes = await suap.getClasses(2019, 1);
+	// var _class = await suap.getClass(48553);
 
-	console.log(_class);
+	console.log(data, year, notes, classes);
 })();
